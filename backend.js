40629e2ebe16f1c5b56ad54edf1c453b81ffbb8a
Andreia Machion <amachion@gmail.com>
2025-10-28 12:46:09 -0300

    apostila 09 até senha criptografada - pagina 54

--- backend.js
+++ backend.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors")
 const mongoose = require("mongoose")
 require('dotenv').config()
+const uniqueValidator = require('mongoose-unique-validator')
+const bcrypt = require("bcrypt")
 const app = express();
 app.use(express.json());
 app.use(cors())
@@ -11,21 +13,14 @@ const Filme = mongoose.model("Filme", mongoose.Schema({
   sinopse: {type: String}
 }))
 
-// let filmes = [
-//   {
-//     titulo: "Forrest Gump - O Contador de Histórias",
-//     sinopse:
-//       "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções.",
-//   },
-//   {
-//     titulo: "Um Sonho de Liberdade",
-//     sinopse:
-//       "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela",
-//   },
-// ];
+const usuarioSchema = mongoose.Schema({
+  login: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+})
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model ("Usuario", usuarioSchema)
 
 //endpoint para devolver a lista de filmes: get /filmes
-
 app.get('/filmes', async (req, res) => {
     const filmes = await Filme.find()
     res.json(filmes)
@@ -36,16 +31,35 @@ app.post('/filmes', async (req, res) => {
   //montar o objeto JSON com as informações enviadas
   const titulo = req.body.titulo
   const sinopse = req.body.sinopse
-
   //montar o filme de acordo com o schema definido, ou seja, um objeto da classe Filme
   const filme = new Filme({titulo: titulo, sinopse: sinopse})
-  
   //salvar no banco
   await filme.save()
-  
   const filmes = await Filme.find()
   res.json(filmes)
 })
+
+app.post('/signup', async (req, res) => {
+  try {
+    //captura informação que o usuário digitou
+    const login = req.body.login
+    const password = req.body.password
+    //vamos encriptar a senha
+    const passwordCriptografada = await bcrypt.hash(password, 10)
+    //monta o objeto json
+    const usuario = new Usuario({login: login, password: passwordCriptografada})
+    //envia para o banco
+    const respostaMongo = await usuario.save()
+    console.log(respostaMongo)
+    res.status(201).end()
+  }
+  catch (exception) {
+    console.log(exception);
+    res.status(409).end()
+  }
+})
+
+
 
 const stringConexao = process.env.CONEXAO_DB
 
